88ff7926ae5685227f112622f5646e577bfe442a
JacksonH <jacksonhole24@gmail.com>
2022-12-23 16:03:13 +1030

    Set-up Player Movement

--- Chop King/Assets/Scripts/PlayerController.cs
+++ Chop King/Assets/Scripts/PlayerController.cs
@@ -5,24 +5,56 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private PlayerInput playerInput;
+    [SerializeField] private InputActionReference moveLeft, moveRight, attack;
 
     [Header("Positions")]
-    public Vector2 playerLeftPos;
-    public Vector2 playerRightPos;
-
-    private void Awake()
-    {
-        
-    }
+    [SerializeField] GameObject playerLeftPos;
+    [SerializeField] GameObject playerRightPos;
 
     void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
+
     }
 
     void Update()
     {
-        
+       
+    }
+
+    public void MoveLeft(InputAction.CallbackContext obj)
+    {
+        Debug.Log("MoveLeft");
+        transform.position = playerLeftPos.transform.position;
+        transform.rotation = new Quaternion(0, 180, 0, 0);
+    }
+
+    public void MoveRight(InputAction.CallbackContext obj)
+    {
+        Debug.Log("MoveRight");
+        transform.position = playerRightPos.transform.position;
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    public void Attack(InputAction.CallbackContext obj)
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        moveLeft.action.performed += MoveLeft;
+
+        moveRight.action.performed += MoveRight;
+
+        attack.action.performed += Attack;
+    }
+
+    private void OnDisable()
+    {
+        moveLeft.action.performed -= MoveLeft;
+
+        moveLeft.action.performed -= MoveRight;
+
+        moveLeft.action.performed -= Attack;
     }
 }
